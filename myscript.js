b006104d62c217922fa303a4e0232e87988614b8
aliasvishnu <aliasvishnu@gmail.com>
2017-11-12 16:36:59 -0800

    Spam classifier yer in Python complete

--- myscript.js
+++ myscript.js
@@ -1,1 +1,16 @@
 // chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
+
+
+// mailContents[0].innerText = "HELLO";
+
+// for(var i = 0; i < 10; i++){
+//     console.log(mailContents.);
+// }
+// for (text in mailContents){
+//     // if(isSpam(text.innerText)){
+//     //     text.innerText = " - **SPAM** " + text.innerText;
+//     // }
+//     console.log(text);
+// }
+
+// chrome.runtime.sendMessage(mailContents[0].innerText);
